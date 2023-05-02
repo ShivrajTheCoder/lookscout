d092b2418efbb549e63ee53671bfac8dbc8ab050
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-05-02 11:20:27 +0530

    final-code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,6 @@
 import '@/styles/globals.css'
-import { StoreProvider } from '@/themecontext/store';
-import { useState } from 'react';
-
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState("light");
   return (
-    <StoreProvider value={{theme,setTheme}}>
       <Component {...pageProps} />
-    </StoreProvider>
   );
 }
